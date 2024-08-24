f27935359f193276e5e20c60b04f095fb5961f22
yotai023 <yotai023333@gmail.com>
2024-08-24 23:32:05 +0900

    課題提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,13 @@
+// 今日の日付を取得
+const today = new Date();
+
+// 年、月、日を取得
+const year = today.getFullYear();
+const month = today.getMonth() + 1; // 月は0から始まるため、1を加える必要があります
+const day = today.getDate();
+
+// 指定された形式で日付を表示
+const formattedDate = `${2023}年${8}月${22}日`;
+
+// console.logで表示
+console.log(formattedDate);
